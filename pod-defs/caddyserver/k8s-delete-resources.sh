30c2b730afcf0f672100a67f20b34e44f9f8ba5d
Guruprasad Nagaraj <gpn@networkart.com>
2019-06-26 12:10:01 +0000

    caddygw is in default namespace.

--- pod-defs/caddyserver/k8s-delete-resources.sh
+++ pod-defs/caddyserver/k8s-delete-resources.sh
@@ -3,5 +3,5 @@
 kubectl delete deployment caddypod
 kubectl delete pods -l app=caddypod
 kubectl delete service k8scaddy
-kubectl delete gateway caddygw -n istio-system
+kubectl delete gateway caddygw
 kubectl delete virtualservice caddyvsvc
